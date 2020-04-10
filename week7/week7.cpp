50e078981b35d2442c6af76dac8ff655ffd0d171
桐 孙 <2731881741@qq.com>
2020-04-10 14:16:12 +0800

    练习2

--- week7/week7.cpp
+++ week7/week7.cpp
@@ -2,10 +2,38 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第七周\\metal-part-distorted-03.png", 1);
+	cv::Mat dstMat;
+	std::vector<cv::Vec2f> lines;
+	int threshold = 50;
+
+	Canny(srcMat, dstMat, threshold, threshold * 3);
+
+	HoughLines(dstMat, lines, 1, CV_PI / 180,100);
+
+	std::vector<cv::Vec2f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it) {
+		float rho = (*it)[0], theta = (*it)[1];
+		cv::Point pt1, pt2;
+		double a = cos(theta);
+		double b = sin(theta);
+		double x0 = a * rho;
+		double y0 = b * rho;
+		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
+		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
+		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
+		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
+		cv::line(srcMat, pt1, pt2, cv::Scalar(0, 0, 255), 1, CV_AA);
+	}
+	cv::imshow("srcMat", srcMat);
+	cv::imshow("dstMat", dstMat);
+	waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
